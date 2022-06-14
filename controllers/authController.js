6ced51e70da8cd03509da60ba3f6380f3d0e65fd
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-06-14 16:43:41 -0300

    fix: adjusting login controller and adding try catch in function body

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,19 +6,23 @@ import sessionRepository from '../repositories/sessionsRepository.js';
 
 export async function login(req, res) {
   const { email, password } = req.body;
-  const users = await usersRepository.getUserByEmail(email);
+  try {
+    const users = await usersRepository.getUserByEmail(email);
 
-  if (users.rowCount === 0) {
-    return res.sendStatus(401);
+    if (users.rowCount === 0) {
+      return res.sendStatus(401);
+    }
+
+    if (bcrypt.compareSync(password, users.rows[0].password)) {
+      const data = { userId: users.rows[0].id };
+      const config = { expiresIn: 60 * 60 * 24 };
+      const token = jwt.sign(data, process.env.JWT_SECRET, config);
+      await sessionRepository.createSession(users.rows[0].id);
+      return res.status(200).send({ token: token, userName: users.rows[0].userName, picture: users.rows[0].picture });
+    }
+    res.sendStatus(401);
+
+  } catch (error) {
+      return res.sendStatus(500);
   }
-
-  if (bcrypt.compareSync(password, users.rows[0].password)) {
-    const data = { userId: users.rows[0].id };
-    const config = { expiresIn: 60 * 60 * 24 };
-    const token = jwt.sign(data, process.env.JWT_SECRET, config);
-    await sessionRepository.createSession(users.rows[0].id);
-    return res.status(200).send({ token: token, userName: users.rows[0].userName, picture: users.rows[0].picture });
-  }
-
-  res.sendStatus(401);
 }
