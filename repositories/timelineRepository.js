70ad61525b1271872173073c065fb88deb008235
heusyfelype <felype.heusy@gmail.com>
2022-06-15 15:22:42 -0300

    feat: melhorada a requisição get da timeline com novas funcionalidades

--- repositories/timelineRepository.js
+++ repositories/timelineRepository.js
@@ -6,12 +6,28 @@ import connection from "../db.js";
 async function getTimeline() {
 
     return connection.query(`
-        SELECT * FROM posts ORDER BY ("createdAt") DESC LIMIT ${LIMIT_POSTS}
-    `);
+        SELECT u."userName", u.picture, p.*, COALESCE(COUNT(l."postId"),0) AS likes
+        FROM posts p
+        LEFT JOIN likes l ON l."postId" = p.id
+        JOIN users u ON u.id = p."userId"
+        GROUP BY (p.id, u.id)
+        ORDER BY p.id DESC`
+    );
+}
+
+async function getAuthTimeLine(id){
+    return connection.query(`
+        SELECT p.*, COALESCE(COUNT(l."postId"),0) AS likes
+        FROM posts p
+        LEFT JOIN likes l ON l."postId" = p.id
+        WHERE l."userId" = $1
+        GROUP BY p.id
+        ORDER BY p.id DESC;`, [id]);
 }
 
 const postsTimeline = {
-    getTimeline
+    getTimeline,
+    getAuthTimeLine
 }
 
 export default postsTimeline;
