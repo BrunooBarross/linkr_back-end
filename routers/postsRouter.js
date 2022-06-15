853ed3fc7cf94abd404cb7824bb21864eab5fc7c
heusyfelype <felype.heusy@gmail.com>
2022-06-15 14:22:02 -0300

    feat: adicionado o middleware de autorização na requisição 'get posts'

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { timeline } from "../controllers/timelineController.js";
+import { validateToken } from "../middlewares/authMiddleware.js";
 
 
 const posts = Router();
 
-posts.get("/timeline", timeline);
+posts.get("/timeline", validateToken, timeline);
 
 export default posts;
