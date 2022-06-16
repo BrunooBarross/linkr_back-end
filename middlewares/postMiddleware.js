07f6a7405d906235b5cb4d442d26f2bf3b816696
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-06-16 15:43:46 -0300

    feat: criando endpoint para delete de posts do próprio usuário, assim como realizando a devidas validações e executanto o delete na tabela post e nas tabelas com relacionamentos

--- middlewares/postMiddleware.js
+++ middlewares/postMiddleware.js
@@ -1,4 +1,5 @@
 import { bodyPostSchema } from "../schemas/postSchema.js";
+import postsTimeline from "../repositories/timelineRepository.js";
 
 export async function verifyPost(req, res, next) {
     const values = req.body;
@@ -10,4 +11,23 @@ export async function verifyPost(req, res, next) {
     }
 
     next();
+}
+
+export async function verifyDeletePost(req, res, next) {
+    const userId = res.locals.userId.id;
+    const idPost = req.headers.id;
+    try {
+        const selectUserId = await postsTimeline.selectUserIdPost(idPost);
+        if(selectUserId.rowCount === 0){
+            return res.sendStatus(404);
+        }
+        if(userId !== selectUserId.rows[0].userId){
+            return res.sendStatus(401);
+        }
+        
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
 }
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { verifyPost } from "../middlewares/postMiddleware.js";
-import { publishPost } from "../controllers/timelineController.js";
+import { verifyPost, verifyDeletePost } from "../middlewares/postMiddleware.js";
+import { deletePost, publishPost } from "../controllers/timelineController.js";
 import { timeline } from "../controllers/timelineController.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
 
@@ -9,5 +9,6 @@ const posts = Router();
 
 posts.get("/timeline", validateToken, timeline);
 posts.post("/post", validateToken, verifyPost, publishPost)
+posts.delete("/post", validateToken, verifyDeletePost, deletePost)
 
 export default posts;
