e7ff9dbf2f0579ccb51a5191f991608d6bc346b6
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-06-22 18:11:43 -0300

    feat: criando end oint para retornar o numero de novos posts

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import validateHashtagsRegex, { verifyPost, verifyDelPutPost } from "../middlewares/postMiddleware.js";
-import { deletePost, publishPost, updatePost } from "../controllers/timelineController.js";
+import { countNewPosts, deletePost, publishPost, updatePost } from "../controllers/timelineController.js";
 import { timeline } from "../controllers/timelineController.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
 
@@ -11,5 +11,6 @@ posts.get("/timeline", validateToken, timeline);
 posts.post("/post", validateToken, verifyPost, validateHashtagsRegex, publishPost)
 posts.delete("/post", validateToken, verifyDelPutPost, deletePost)
 posts.put("/post", validateToken, verifyDelPutPost, validateHashtagsRegex, updatePost)
+posts.get("/count/posts", validateToken, countNewPosts)
 
 export default posts;
