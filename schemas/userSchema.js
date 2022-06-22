a7905f1bf7cc9e46054a93cefac0b26774cc2509
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-06-22 13:40:33 -0300

    fix: ajustes regex userSchema signUp

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,7 +4,7 @@ export const signUpSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(4).required(),
     userName: joi.string().min(4).required(),
-    picture: joi.string().regex(/(https:\/\/)([^\s(["<,>/]*)(\/)[^\s[",><]*(.png|.jpg)(\?[^\s[",><]*)?/).required()
+    picture: joi.string().regex(/(https:\/\/|http:\/\/)([^\s(["<,>]*)(\/)[^\s[",><]*(.png|.jpg)(\?[^\s[",><]*)?/).required()
 });
 
 export const signInSchema = joi.object({
